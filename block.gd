5f91a51b8f6645a7820c1949c45e5ea1439c24b1
boflynn <boflynn@gmail.com>
2025-03-26 18:51:20 -0400

    Add bullet interactions

--- block.gd
+++ block.gd
@@ -7,8 +7,24 @@ var health: int
 func _ready() -> void:
 	$Background.color = colors[randi() % colors.size()]
 	health = randi_range(get_parent().min_block_health, get_parent().max_block_health)
-	$HealthLabel.text = str(health)
+	update_health_label()
 
 func _process(delta: float) -> void:
 	# getting bullet_speed like this feels awful
 	position.y += get_parent().block_speed * delta
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		health -= 1
+		update_health_label()
+		get_parent().increase_score()
+		
+		# destroy the bullet as well
+		area.queue_free()
+		
+		if health <= 0:
+			queue_free()
+
+func update_health_label() -> void:
+	$HealthLabel.text = str(health)
--- main.gd
+++ main.gd
@@ -1,13 +1,38 @@
 extends Node
 
 @export var bullet : PackedScene
+@export var block : PackedScene
+
 var bullet_speed: int = 500
+
 var block_speed: int = 150
+var block_size: int = 108
+
 var min_block_health: int = 10
 var max_block_health: int = 20
+var blocks_to_spawn: int = 2
+
+var score: int = 0
+func increase_score() -> void:
+	score += 1
+	$UI/ScoreLabel.text = "Score: %s" % score
 
 func _on_fire_rate_timer_timeout() -> void:
 	var bullet_instance: Area2D = bullet.instantiate()
 	bullet_instance.position = $Player.position
 	
 	add_child(bullet_instance)
+
+func _on_block_spawn_timer_timeout() -> void:
+	var positions = [0,1,2,3,4]
+	positions.shuffle()
+	var index: int = 0
+	
+	while index < blocks_to_spawn:
+		var block_instance: Control = block.instantiate()
+		block_instance.position.x = positions[index] * block_size
+		block_instance.position.y = -block_size
+		
+		add_child(block_instance)
+		index += 1
+	
